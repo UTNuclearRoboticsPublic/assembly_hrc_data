d184c1d0f4bec290f1aac095633cec2e9f1faada
shondle <106922533+shondle@users.noreply.github.com>
2023-01-14 01:29:15 -0600

    Add labels, create dataset, update dataloader
    Added sample frames with json annotations. Added way to convert json and img to VOC-format from labelme semantic segmentation docs. Updated dataloader.py, still has errors, but fixes will be easier with conversion updates.

--- dataloader.py
+++ dataloader.py
@@ -6,9 +6,11 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import numpy as np
 import math
+import os
+import cv2
 
-transforms = transforms.compose ({
-    transforms.resize((224, 224)),
+transforms = transforms.Compose ({
+    transforms.Resize((224, 224)),
     # random changes so it doesn't learn biased (only facing one direction)
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(10),
@@ -19,14 +21,15 @@ transforms = transforms.compose ({
     # transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
 })
 
-dataset = torchvision.datasets.ImageFolder(root='./train', transform = transforms)
+# dataset = torchvision.datasets.ImageFolder(root='./train', transform = transforms)
 
 class AssemblyDataset(Dataset):
     def __init__(self):
         # data loading
-        data = np.loadtxt()
-        self.images = 1
-        self.masks = 1
+        image_dir = 'images'
+        image_files = os.listdir(r'./Labelled/train/images')
+        self.images = [cv2.imread(os.path.join(image_dir, file)) for file in image_files if file.endswith('.png')]
+        self.masks = os.path.join(r"./Labelled", r"/train/labels")
 
     def __getitem__(self, index):
         # dataset[0]
