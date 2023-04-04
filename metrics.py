e0433e04ca25263d105759dc093e70ec5d22ee50
Shivansh <106922533+shondle@users.noreply.github.com>
2023-04-03 23:44:37 -0500

    Add IOU and edit ACE

--- metrics.py
+++ metrics.py
@@ -9,8 +9,14 @@ def expected_calibration_error(predictions, targets):
     bce = metric(predictions, targets)
     return bce
 
+def iou(outputs, targets, device="cuda"):
+    metric = BinaryJaccardIndex().to(device=device)
+    return metric(outputs, targets)
+
 # Adaptive calibration error
 def adaptive_calibration_error(predictions, targets):
+    # make sure to change this part of the code to get average adaptive calibration error
+    predictions = predictions[0, :, :, :]
     predictions2 = (predictions>0.5).float().cpu().numpy()
     predictions = predictions.cpu().numpy()
     targets = targets.cpu().numpy()
@@ -32,7 +38,7 @@ def adaptive_calibration_error(predictions, targets):
 
 
     # will never be less than zero when we take a sigmoid
-    # probabilities[probabilities < 0] = 0
+    probabilities[probabilities < 0] = 0
 
     pred_matrix = np.zeros([rows, columns])
     label_matrix = np.zeros([rows, columns])
@@ -41,11 +47,15 @@ def adaptive_calibration_error(predictions, targets):
     print(f"datatype is {(predictions).astype(np.int32).dtype}")
 
     pred_matrix[np.arange(rows), predictions.astype(np.int32)] = 1
-    label_matrix[np.arange(rows), targets] = 1
+    label_matrix[np.arange(rows), targets.astype(np.int32)] = 1
+
+    acc_matrix = np.equal(pred_matrix, label_matrix)
+
+    probabilities = probabilities[:, 0]
 
     # if the probabilities array is empty (no confidence score)
     if probabilities.size == 0:
-        boundaries = np.linspace(0, 1, bins + 1)
+        boundaries = np.linspace(0, 1,  bins + 1)
         low_bounds = boundaries[:-1]
         high_bounds = boundaries[1:]
     # if prob. array not empty create bounds equally spaced 
@@ -66,13 +76,20 @@ def adaptive_calibration_error(predictions, targets):
     bin_confidence = np.zeros(bins)
     bin_score = np.zeros(bins)
 
-    confidences = confidences
-    accuracies = accuracies
+    # confidences = confidences[:, 0]
+    # accuracies = accuracies[:, 0]
+    confidences = probabilities[:, 0]
+    accuracies = acc_matrix[:, 0]
+
+    print(f" the lower is {low_bounds.size} and high is {high_bounds.size}")
 
     for i, (lower_bound, upper_bound) in enumerate(zip(low_bounds, high_bounds)):
         # boolean masks for confidence values below upper and above lower bound
         in_bin = np.greater(confidences, lower_bound.item()) * np.less_equal(confidences, upper_bound.item())
-        bin_proportion[i] = np.mean(in_bin)
+        if np.any(in_bin):
+            bin_proportion[i] = np.mean(in_bin)
+        else:
+            break
 
         # calc |acc(r, k) - conf(r, k)|
         if bin_proportion[i].item()>0:
@@ -105,6 +122,14 @@ def shannon_entropy(probabilities):
     avg_entropy_per_image = np.average(entropy_list)
     return avg_entropy_per_image
 
-def variance(probabilities):
+def avg_variance_per_image(probabilities):
     # check which dimension you need to do this over
-    return torch.var(probabilities, dim=0)
+    var = []
+    num_batches = ((probabilities[:, 0, 0, 0].shape))[0]
+    for i in range(num_batches):
+        probability = probabilities[i, :, :, :].cpu().numpy()
+        probability = probability.flatten()
+        variance = np.var(probability)
+        var.append(variance)
+
+    return np.average(var)
